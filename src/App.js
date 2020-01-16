fbe2fa551f8996f217fffd891a6501c6c41192c5
Alan Dega <arkidega@gmail.com>
2020-01-15 20:29:46 -0600

    primer cambio en rama rest

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import logo from './assets/spacexlogo.png'
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Lanzamientos from './components/Lanzamientos';
+import Launches from './components/Launches';
 import Missions from './components/Missions';
 import Rockets from './components/Rockets';
 import Home  from './components/Home';
@@ -11,23 +10,19 @@ import Home  from './components/Home';
 
 
 
-const client = new ApolloClient ({
-  uri: 'http://localhost:5000/graphql'
-})
+
 
 function App() {
   return (
     
-    <ApolloProvider client={client}>
       <BrowserRouter>
         <Switch>
           <Route exact path ="/" component={Home} />
-          <Route exact path="/lanzamientos" component={Lanzamientos} />
+          <Route exact path="/lanzamientos" component={Launches} />
           <Route exact path="/misiones" component={Missions} />
           <Route exact path="/cohetes" component={Rockets} />
         </Switch>
       </BrowserRouter>
-    </ApolloProvider>
   );
 }
 
